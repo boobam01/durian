9cca1996be1d8ba8b3cd1eb70297d40cf0bbcfc1
htaox <htaox@hotmail.com>
2016-01-31 06:18:50 -0500

    GetBOTypes works with selectica.h

--- selectica.h
+++ selectica.h
@@ -10,13 +10,34 @@ using namespace durian;
 
 namespace selectica {
   
-  /**
-    Template generator
-  **/
+  template<typename socketType>
   class client {
     durian::generator<XmlElement::Message> generator;
+    unique_ptr<api::client<socketType>> apiClient;
+    PlustacheTypes::ObjectType user, password, token, response;
+    shared_ptr<Plustache::Context> ctx;
+    std::map<string, string> customHeaders;
+    string host, servicePath, userstr, passwordstr;    
   public:
     client(){}
+    client <socketType>(const char* _host,
+      const char* _servicePath,
+      const char* _user,
+      const char* _password,
+      std::map<string, string>& _customHeaders) : host(_host), servicePath(_servicePath), userstr(_user), passwordstr(_password), customHeaders(_customHeaders) {
+
+      // create api::client<socketType>
+      apiClient = make_unique<api::client<SimpleWeb::HTTP>>(host);
+
+      // create the context that will be shared by all templates
+      ctx = make_shared<Plustache::Context>();
+
+      // store user & password in the context
+      user["user"] = userstr;
+      password["password"] = passwordstr;
+      (*ctx).add(user);
+      (*ctx).add(password);
+    }
     ~client(){}
 
     shared_ptr<string> loginMsg(shared_ptr<Plustache::Context> ctx) {
@@ -34,41 +55,56 @@ namespace selectica {
     shared_ptr<string> getBOMetaDataMsg(shared_ptr<Plustache::Context> ctx) {
       return this->generator.render(ctx, "tpl/selectica/getBOMetaData", "tpl/selectica/root");
     }
-  };
 
-  /**
-    API client with promises
-  **/
-  template<typename socketType>
-  class api {
-    
-  public:
-    api<socketType>(const char* _host,
-      const char* _servicePath,
-      const char* _user,
-      const char* _password,
-      std::map<string, string>& _customHeaders) : host(_host), servicePath(_servicePath), user(_user), password(_password), customHeaders(_customHeaders) {
+    shared_ptr<Plustache::Context> selectica::client<socketType>::GetBOTypes() {
 
-      // create api::client<socketType>
-      apiClient = make_unique<api::client<SimpleWeb::HTTP>>(host);
+      // Login response
+      auto loginResponse = [&](boost::future<std::string> f){
+        auto res = f.get();        
+        // xml => json
+        auto raw = xml2json(res.c_str());
+        // parse the message
+        rapidjson::Document d;
+        d.Parse(raw.c_str());
+        dumpPrettyJson("LoginResponse.js", d);
+        // get the token from the message
+        auto tok = d["Envelope"]["Body"]["LoginResponse"]["SessionToken"].GetString();
+        // add token to context
+        token["token"] = tok;
+        (*ctx).add(token);
 
-      // create selectica::client for action message generation
-      client = make_unique<selectica::client>();
+        return string(tok);
+      };
 
-      // store user & password in the context
-      user["user"] = _user;
-      password["password"] = _password;
-      (*ctx).add(user);
-      (*ctx).add(password);
+      // GetBOTypes
+      auto getBOTypes = [&](boost::future<std::string> fut){
 
-    }
-    ~api(){}
+        // the session token is already stored in the context (ctx)
+        auto getBOTypesXml = this->getBOTypesMsg(ctx);        
+        // future factory
+        auto future2 = apiClient->methodPromise["POST"];        
+        // create the future
+        auto f2 = future2(servicePath, *getBOTypesXml, customHeaders);
 
-    string GetBOTypes() {
+        return f2.get();
+      };
+
+      //GetBOTypes response
+      auto getBOTypesResponse = [&](boost::future<std::string> fut){
+        
+        auto res = fut.get();
+        auto raw = xml2json(res.c_str());
+        //save response in the context, this will be used by the caller
+        response["response"] = raw;
+        (*ctx).add(response);
+
+        return raw;
+      };
 
       // Logout
       auto logout = [&](boost::future<std::string> f){
-        auto logoutXml = client.logoutMsg(ctx);
+        // the session token is in the ctx, it will be sent to the server to end session
+        auto logoutXml = this->logoutMsg(ctx);
         auto future = apiClient->methodPromise["POST"];
         auto pms = future(servicePath, *logoutXml, customHeaders);
         return pms.get();
@@ -80,69 +116,10 @@ namespace selectica {
         return res;
       };
 
-      // Login response
-      auto loginResponse = [&](boost::future<std::string> f){
-        auto res = f.get();
-        dumpFile("LoginResponse.xml", res.c_str());
-
-        auto raw = xml2json(res.c_str());
-
-        dumpFile("LoginResponse.xml", res.c_str());
-
-        // parse the message
-        rapidjson::Document d;
-        d.Parse(raw.c_str());
-        dumpPrettyJson("LoginResponse.js", d);
-
-        // get the token from the message
-        auto tok = d["Envelope"]["Body"]["LoginResponse"]["SessionToken"].GetString();
-
-        // add token to context
-        token["token"] = tok;
-        (*ctx).add(token);
-
-        return string(tok);
-      };
-
-      // GetBOTypes
-      auto getBOTypes = [&](boost::future<std::string> fut){
-
-        auto tok = fut.get();
-        //ctx.get()->get("token")
-        // getBOTypes -> token should appear in XML b/c context
-        auto getBOTypesXml = client.getBOTypesMsg(ctx);
-
-        // test regex
-        // captures not working so well in C++ 11
-        *getBOTypesXml = std::regex_replace(*getBOTypesXml, sessRgx, "<SessionToken>" + tok + "</SessionToken>");
-
-        dumpFile("GetBOTypes.xml", (*getBOTypesXml).c_str());
-
-        auto future2 = apiClient->methodPromise["POST"];
-        auto f2 = future2(servicePath, *getBOTypesXml, customHeaders);
-
-        return f2.get();
-      };
-
-      //GetBOTypes response
-      auto getBOTypesResponse = [&](boost::future<std::string> fut){
-        auto res = fut.get();
-
-        dumpFile("GetBOTypesResponse.xml", res.c_str());
-
-        auto raw = xml2json(res.c_str());
-        rapidjson::Document d;
-        d.Parse(raw.c_str());
-
-        dumpPrettyJson("GetBOTypesResponse.js", d);
-
-        return raw;
-      };
-
       // get a login future
       auto login = apiClient->methodPromise["POST"];
       // get login template
-      auto loginXml = client->loginMsg(ctx);
+      auto loginXml = this->loginMsg(ctx);
       // start the promises
       auto promises = login(servicePath, *loginXml, customHeaders)
         .then(loginResponse)
@@ -151,32 +128,11 @@ namespace selectica {
         .then(logout)
         .then(logoutResponse);
 
-      auto resp =  promises.get();
+      // wait
+      auto resp = promises.get();
 
-      cout << resp << "\n";
-
-      return resp;
+      return ctx;
     }
-
-    /**
-      // Experimental
-    
-    typedef std::function<string(string, string)> Funcp = func<string>;
-    template<typename T>
-    string chainPromises(Funcp f0, Funcp f1, Funcp f2) {
-
-      return "";
-    }
-    **/
-    
-  private:
-    unique_ptr<api::client<socketType>> apiClient;
-    unique_ptr<selectica::client> client;
-    PlustacheTypes::ObjectType user, password, token;
-    shared_ptr<Plustache::Context> ctx;
-    std::map<string, string> customHeaders;
-    string host, servicePath;
-    
   };
 
 }
