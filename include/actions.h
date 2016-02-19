2b58d7dc8adb07be134b4b5ecc9afd92c8858675
htaox <htaox@hotmail.com>
2016-02-19 15:24:32 -0500

    Corrected template for createAction

--- include/actions.h
+++ include/actions.h
@@ -10,17 +10,17 @@ namespace {
     CONTEXT c_;
     T t_;
   public:
-    Composer(FUNC f, CONTEXT c, T t) : f_(f), c_(c), t(t_) {}
+    Composer(FUNC f, CONTEXT c, T t) : f_(f), c_(c), t_(t) {}
 
     template <typename CTX, typename T>
-    CTX operator() (const CTX& x, T& t) const {
-      return f_(x);
+    CTX operator() (CTX& x, T& t) const {
+      return f_(x, t);
     }
   };
 
   template <typename F, typename C, typename T>
-  Composer<F,C,T> createAction(F f, C c, T t) {
-    return Composer<F>(f, t);
+  Composer<F,C,T> createAction(const F f, C c, T t) {
+    return Composer<F,C,T>(f, c, t);
   }
 
 }
