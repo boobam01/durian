7abc7aa99e0ffaad1271d882647e68cf9649fc07
htaox <htaox@hotmail.com>
2016-03-16 15:22:58 -0400

    api_server.h not starting

--- include/api_server.h
+++ include/api_server.h
@@ -14,7 +14,7 @@ namespace api {
   struct Route {
     string expression;
     string verb;
-    std::function<void(typename SimpleWeb::ServerBase<T>::Reponse&, shared_ptr<typename SimpleWeb::ServerBase<T>::Request>)> action;
+    std::function<void(typename SimpleWeb::ServerBase<T>::Response&, shared_ptr<typename SimpleWeb::ServerBase<T>::Request>)> action;
   };
 
   template<typename T>
@@ -33,24 +33,20 @@ namespace api {
       }
       connect(routes);
     }
-    //Start server
     void start() {
-      std::thread server_thread([&(*_server)](){
-        (*_server).start();
-      });
-      //Wait for server to start so that the client can connect
-      std::this_thread::sleep_for(chrono::seconds(1));
+      _server->start();
     }
   private:
     int port, threads;
     std::string certFile, privateKeyFile;
     shared_ptr<SimpleWeb::Server<T>> _server;
+    
     void connect(vector<Route<T>>& routes) {
-      std::for_each(routes.begin(), routes.end(), [](const Route& r){
-        (*_server)->resource[r.expression][r.verb] = r.action;
+      std::for_each(routes.begin(), routes.end(), [this](const Route<T>& r){
+        _server->resource[r.expression][r.verb] = r.action;
       });
       //default
-      (*_server)->default_resource["GET"] = [](SimpleWeb::ServerBase<T>::Response& response, shared_ptr<SimpleWeb::ServerBase<T>::Request> request) {
+      _server->default_resource["GET"] = [](SimpleWeb::ServerBase<T>::Response& response, shared_ptr<SimpleWeb::ServerBase<T>::Request> request) {
         response << "HTTP/1.1 200 OK\r\nContent-Length: " << 0 << "\r\n\r\n";
       };
     }
