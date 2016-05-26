3ddf96b53ed32227ca11955e40a997d257c26c83
htaox <htaox@hotmail.com>
2016-05-26 13:24:24 -0400

    Add bulkload function to mongoClient

--- include/mongoClient.h
+++ include/mongoClient.h
@@ -20,7 +20,7 @@ namespace mongoclient {
     int connect() {
       std::string uri = host + ":" + port;
       try {
-        conn.connect(uri);        
+        conn.connect(uri);
       }
       catch (const mongo::DBException &e){
         spdlog::get("logger")->error() << "failed to initialize the client driver";
@@ -29,8 +29,8 @@ namespace mongoclient {
       return 0;
     }
 
-    std::auto_ptr<mongo::DBClientCursor> find(const string collection, mongo::Query cri, int limit, int skip, mongo::BSONObj* selector){      
-      auto cur = conn.query(database + "." + collection, cri, limit, skip, selector, 0, 0);      
+    std::auto_ptr<mongo::DBClientCursor> find(const string collection, mongo::Query cri, int limit, int skip, mongo::BSONObj* selector){
+      auto cur = conn.query(database + "." + collection, cri, limit, skip, selector, 0, 0);
       return cur;
     }
 
@@ -41,13 +41,46 @@ namespace mongoclient {
     }
 
     std::function<boost::future<std::string>(const string, mongo::Query, mongo::BSONObj*)> findOnePromise() {
-      return [this](const string collection, mongo::Query cri, mongo::BSONObj* selector)->boost::future<string> {
+      return [this](const string collection, mongo::Query cri, mongo::BSONObj* selector)->boost::future < string > {
         return boost::async([=]()->string {
           auto resp = conn.findOne(database + "." + collection, cri, selector, 0);
           auto o = resp.jsonString(mongo::Strict, 0, false);
           return o;
         });
       };
+    }
+
+    /**
+    MongoDB bulk operation
+    **/
+    string bulkToMongo(const string database, const string collection, vector<string> v) {
+      string resp = "";
+      string ns = database + "." + collection;
+      
+      auto bulk = this->conn.initializeUnorderedBulkOp(ns);
+      for (auto& w : v) {
+        auto o = mongo::fromjson(w);
+        if (o.hasField("_id")) {
+          auto v = o["_id"];
+          auto nb = v.wrap();
+          bulk.find(nb).upsert().replaceOne(o);
+        }
+      }
+      mongo::WriteConcern wc;
+      mongo::WriteResult wr;
+      bulk.execute(&wc, &wr);
+
+      if (wr.hasWriteErrors()){
+        for (auto& bo : wr.writeErrors()){
+          resp += "Bulk upsert for ns => " + ns + " writeError => " + bo.jsonString() + "\n";
+        }
+      }
+      auto num = wr.nUpserted();
+      auto num1 = wr.nModified();
+      stringstream ss;
+      ss << "Bulk upsert for ns => " << ns << " nUpserted => " << num << " nModified => " << num1 << " total => " << (num + num1);
+      resp += ss.str() + "\n";
+      return resp;
     }
 
   };
